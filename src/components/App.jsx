032efab4196eb66691df94680ef62aca6f3aeb5e
DiligentViking <acore-n@proton.me>
2026-06-11 17:10:32 -0500

    Add provideRandomOptions function
    It is fully featured, with an onlyUnique option complete with error-handling.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,40 @@ import { useState } from 'react';
 import { Scoreboard } from './Scoreboard.jsx';
 import { Gameboard } from './Gameboard.jsx';
 
+function provideRandomOptions(
+  array,
+  onlyUnique = true,
+  outputLength = array.length,
+) {
+  const outputArray = [];
+  const remainingOptions = [...array];
+
+  for (let i = 0; i < outputLength; i++) {
+    let randomIndex;
+    let randomItem;
+
+    if (onlyUnique) {
+      do {
+        if (!remainingOptions.length && outputLength - i) {
+          console.error(
+            'Error: Out of unique options. Do not make outputLength higher than number of unique elements in input array.',
+          );
+          return;
+        }
+        randomIndex = Math.floor(Math.random() * remainingOptions.length);
+        randomItem = remainingOptions.splice(randomIndex, 1)[0];
+      } while (outputArray.includes(randomItem));
+    } else {
+      randomIndex = Math.floor(Math.random() * array.length);
+      randomItem = array[randomIndex];
+    }
+
+    outputArray.push(randomItem);
+  }
+
+  return outputArray;
+}
+
 export default function App() {
   const [clickedPokes, setClickedPokes] = useState([]);
   const [bestScore, setBestScore] = useState(0);
