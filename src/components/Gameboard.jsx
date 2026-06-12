cde65816d0245e1babd6bbe2c0c8ccb43924b33f
DiligentViking <acore-n@proton.me>
2026-06-12 10:44:01 -0500

    Refactor Gameboard to use a list

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -3,26 +3,16 @@ import '../styles/Gameboard.css';
 export function Gameboard({ pokeArray, handlePokeClick }) {
   return (
     <div className='gameboard'>
-      <button onClick={handlePokeClick} data-poke-name={pokeArray[0]}>
-        {pokeArray[0]}
-      </button>
-      <br />
-      <br />
-      <button onClick={handlePokeClick} data-poke-name={pokeArray[1]}>
-        {pokeArray[1]}
-      </button>
-      <br />
-      <br />
-      <button onClick={handlePokeClick} data-poke-name={pokeArray[2]}>
-        {pokeArray[2]}
-      </button>
-      <br />
-      <br />
-      <button onClick={handlePokeClick} data-poke-name={pokeArray[3]}>
-        {pokeArray[3]}
-      </button>
-      <br />
-      <br />
+      {pokeArray.map((poke) => (
+        <button
+          key={poke}
+          className='poke-card'
+          onClick={handlePokeClick}
+          data-poke-name={poke}
+        >
+          {poke}
+        </button>
+      ))}
     </div>
   );
 }
