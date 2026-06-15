9745c5c33c77651faae1cf870c978bfcf8be38a9
DiligentViking <acore-n@proton.me>
2026-06-15 11:14:45 -0500

    Prepare to make complete pokeData object

--- src/constants/pokeData.js
+++ src/constants/pokeData.js
@@ -1,22 +1,95 @@
-export const POKE_CHOICES = [
-  'surskit',
-  'raltz',
-  'latias',
-  'latios',
-  'mega latios',
-  'morpeko',
-  'leopard',
-  'purloin',
-  'zygard',
-  'bulbasaur',
-  'deoxys',
-  'eevee',
-  'cinderace',
-  'umbreon',
-  'jolteon',
-  'alolan vulpix',
-  'darkrai',
-  'sawsbuck',
-  'furret',
-  'lugia',
-];
+const pokeData = {
+  surskit: {
+    displayName: '',
+    urlName: '',
+  },
+  raltz: {
+    displayName: '',
+    urlName: '',
+  },
+  latias: {
+    displayName: '',
+    urlName: '',
+  },
+  latios: {
+    displayName: '',
+    urlName: '',
+  },
+  megaLatios: {
+    displayName: 'Mega Latios',
+    urlName: 'latios-mega',
+  },
+  morpeko: {
+    displayName: '',
+    urlName: 'morpeko-hangry',
+  },
+  leopard: {
+    displayName: 'Mega Latios',
+    urlName: 'latios-mega',
+  },
+  purloin: {
+    displayName: 'Mega Latios',
+    urlName: 'latios-mega',
+  },
+  zygarde: {
+    displayName: '',
+    urlName: 'zygarde-complete',
+  },
+  bulbasaur: {
+    displayName: '',
+    urlName: '',
+  },
+  deoxys: {
+    displayName: '',
+    urlName: 'deoxys-attack',
+  },
+  eevee: {
+    displayName: '',
+    urlName: '',
+  },
+  cinderace: {
+    displayName: '',
+    urlName: '',
+  },
+  umbreon: {
+    displayName: '',
+    urlName: '',
+  },
+  jolteon: {
+    displayName: '',
+    urlName: '',
+  },
+  alolanVulpix: {
+    displayName: 'Alolan Vulpix',
+    urlName: 'vulpix-alola',
+  },
+  darkrai: {
+    displayName: '',
+    urlName: '',
+  },
+  sawsbuck: {
+    displayName: '',
+    urlName: '',
+  },
+  furret: {
+    displayName: '',
+    urlName: '',
+  },
+  lugia: {
+    displayName: '',
+    urlName: '',
+  },
+};
+
+function addPoke(name, displayName = null, urlName = null) {
+  pokeData;
+}
+
+export const POKE_CHOICES = Object.keys(pokeData);
+
+// An object with poke names as keys
+// The values will be objects with urls and aliases and stuff
+// The data is exported
+
+// an empty displayName makes it use the key capitalized
+// an empty urlName makes it use the key
