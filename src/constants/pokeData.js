eb54e28534a6ac26a1b9e19a9caec38b7e2f340b
DiligentViking <acore-n@proton.me>
2026-06-15 12:01:10 -0500

    Have pokeData use id's instead of strings for f...
    ...etching data

--- src/constants/pokeData.js
+++ src/constants/pokeData.js
@@ -2,33 +2,33 @@ import { toCapitalCase } from '../utils/stringHelpers.js';
 
 const pokeData = {};
 
-addPoke('surskit');
-addPoke('raltz');
-addPoke('latias');
-addPoke('latios');
-addPoke('megaLatios', 'Mega Latios', 'latios-mega');
-addPoke('morpeko', null, 'morpeko-hangry');
-addPoke('leopard');
-addPoke('purloin');
-addPoke('zygarde', null, 'zygarde-complete');
-addPoke('bulbasaur');
-addPoke('deoxys');
-addPoke('eevee');
-addPoke('cinderace');
-addPoke('umbreon');
-addPoke('jolteon');
-addPoke('alolanVulpix', 'Alolan Vulpix', 'vulpix-alola');
-addPoke('darkrai');
-addPoke('sawsbuck');
-addPoke('furret');
-addPoke('lugia');
+addPoke('surskit', 283);
+addPoke('ralts', 280);
+addPoke('latias', 380);
+addPoke('latios', 381);
+addPoke('megaLatios', 10063, 'Mega Latios');
+addPoke('morpeko', 10187);
+addPoke('liepard', 510);
+addPoke('purrloin', 509);
+addPoke('zygarde', 10120);
+addPoke('bulbasaur', 1);
+addPoke('deoxys', 10001);
+addPoke('eevee', 133);
+addPoke('cinderace', 815);
+addPoke('umbreon', 197);
+addPoke('jolteon', 135);
+addPoke('alolanVulpix', 10103, 'Alolan Vulpix');
+addPoke('darkrai', 491);
+addPoke('sawsbuck', 586);
+addPoke('furret', 162);
+addPoke('lugia', 249);
 
 console.log(pokeData);
 
-function addPoke(name, displayName = null, urlName = null) {
+function addPoke(name, id, displayName = null) {
   pokeData[name] = {
+    id,
     displayName: displayName ?? toCapitalCase(name),
-    urlName: urlName ?? name, // May use in future for pokeapi.co calls
   };
 }
 
