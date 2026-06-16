5272ee703c3504aba1a8650b320a22ba310e71be
DiligentViking <acore-n@proton.me>
2026-06-16 10:07:37 -0500

    Refactor pokeData to just be a plain object

--- src/constants/pokeData.js
+++ src/constants/pokeData.js
@@ -1,40 +1,28 @@
-import { toCapitalCase } from '../utils/stringHelpers.js';
-
-const pokeData = {};
-
-addPoke('surskit', 283);
-addPoke('ralts', 280);
-addPoke('latias', 380);
-addPoke('latios', 381);
-addPoke('megaLatios', 10063, 'Mega Latios');
-addPoke('morpeko', 10187);
-addPoke('liepard', 510);
-addPoke('purrloin', 509);
-addPoke('zygarde', 10120);
-addPoke('bulbasaur', 1);
-addPoke('deoxys', 10001);
-addPoke('eevee', 133);
-addPoke('cinderace', 815);
-addPoke('umbreon', 197);
-addPoke('jolteon', 135);
-addPoke('alolanVulpix', 10103, 'Alolan Vulpix');
-addPoke('darkrai', 491);
-addPoke('sawsbuck', 586);
-addPoke('furret', 162);
-addPoke('lugia', 249);
-
-console.log(pokeData);
-
-function addPoke(name, id, displayName = null) {
-  pokeData[name] = {
-    id,
-    displayName: displayName ?? toCapitalCase(name), // Can abstract further, turn camel case into separate words
-  };
-}
+const POKE_DATA = {
+  surskit: 283,
+  ralts: 280,
+  latias: 380,
+  latios: 381,
+  megaLatios: 10063,
+  morpeko: 10187,
+  liepard: 510,
+  purrloin: 509,
+  zygarde: 10120,
+  bulbasaur: 1,
+  eevee: 133,
+  cinderace: 815,
+  umbreon: 197,
+  jolteon: 135,
+  alolanVulpix: 10103,
+  darkrai: 491,
+  sawsbuck: 586,
+  furret: 162,
+  lugia: 249,
+};
 
 export function createImgUrl(pokeName) {
-  const pokeId = pokeData[pokeName].id;
+  const pokeId = POKE_DATA[pokeName];
   return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`;
 }
 
-export const POKE_CHOICES = Object.keys(pokeData);
+export const POKE_CHOICES = Object.keys(POKE_DATA);
